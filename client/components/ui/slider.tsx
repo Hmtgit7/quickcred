fc5e825b8bd8650a15f1a5077d72f608174fadd5
Hmtgit7 <hmtloharcoding3579@gmail.com>
2026-05-17 07:28:56 +0530

    feat(slider): update styles for track and thumb components to enhance visual consistency

--- client/components/ui/slider.tsx
+++ client/components/ui/slider.tsx
@@ -38,18 +38,18 @@ function Slider({
     >
       <SliderPrimitive.Track
         data-slot="slider-track"
-        className="relative grow overflow-hidden rounded-full bg-muted data-horizontal:h-2 data-horizontal:w-full data-vertical:h-full data-vertical:w-2"
+        className="relative grow overflow-hidden rounded-full border border-black/20 bg-black/10 dark:border-white/30 dark:bg-white/25 data-horizontal:h-2.5 data-horizontal:w-full data-vertical:h-full data-vertical:w-2.5"
       >
         <SliderPrimitive.Range
           data-slot="slider-range"
-          className="absolute bg-primary select-none data-horizontal:h-full data-vertical:w-full"
+          className="absolute bg-emerald-600 dark:bg-white select-none data-horizontal:h-full data-vertical:w-full"
         />
       </SliderPrimitive.Track>
       {Array.from({ length: _values.length }, (_, index) => (
         <SliderPrimitive.Thumb
           data-slot="slider-thumb"
           key={index}
-          className="relative block size-5 shrink-0 rounded-full border-2 border-primary bg-background shadow-sm ring-ring/40 transition-[color,box-shadow,transform] select-none after:absolute after:-inset-2 hover:ring-4 focus-visible:ring-4 focus-visible:outline-hidden active:scale-95 active:ring-4 disabled:pointer-events-none disabled:opacity-50"
+          className="relative block size-5 shrink-0 rounded-full border-2 border-emerald-600 bg-emerald-600 shadow-sm ring-ring/40 transition-[color,box-shadow,transform] select-none after:absolute after:-inset-2 hover:ring-4 focus-visible:ring-4 focus-visible:outline-hidden active:scale-95 active:ring-4 disabled:pointer-events-none disabled:opacity-50 dark:border-white dark:bg-white"
         />
       ))}
     </SliderPrimitive.Root>
